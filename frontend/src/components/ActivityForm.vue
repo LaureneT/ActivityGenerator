737c68c3d4d3eda4e21ae607f12b20f99c0155f9
Laurène Tavella <59617674+LaureneT@users.noreply.github.com>
2023-07-28 14:22:25 -0400

    add multiple constraints and their config to a new activity

--- frontend/src/components/ActivityForm.vue
+++ frontend/src/components/ActivityForm.vue
@@ -7,15 +7,17 @@
         <input v-model="name" type="text" />
         </div>
 
-        <div>
-            <label>Constraint: </label>
-            <select id="constraintDropdown" @change="onConstraintDropdownChange" v-model="selectedConstraint">
+        <button type="button" @click="addConstraint">+ Add a constraint to the activity</button>
+        <div v-for="(constraint, index) in constraintInstances" :key="index">
+          <label>Constraint: </label>
+          <select v-model="constraint.selectedConstraint" @change="onConstraintDropdownChange(index)">
             <option v-for="option in constraints" :value="option" :key="option">{{ option.name }}</option>
-            </select>
-        </div>
+          </select>
+          <button type="button" @click="removeConstraint(index)">-</button>
 
-        <!-- Show fields based on the selected type of operator -->
-        <div id="inputConfigValuesContainer"></div>      
+          <!-- Show fields based on the selected type of operator -->
+          <div :id="'inputConfigValuesContainer' + index"></div>      
+        </div>
       </form>
     </div>
   </template>
@@ -32,6 +34,7 @@
         selectedConstraint: '',
         selectedOperator: null,
         constraintsConfig: {},
+        constraintInstances: [],
       };
     },
     methods: {
@@ -53,22 +56,24 @@
           }
         },
         // eslint-disable-next-line
-        onConstraintDropdownChange(event){
-            this.selectedOperator = GetOperatorWithSymbol(this.selectedConstraint.type);
-            const valuesContainer = document.getElementById('inputConfigValuesContainer');
+        onConstraintDropdownChange(index){
+          this.selectedConstraint = this.constraintInstances[index].selectedConstraint;
+          this.selectedOperator = GetOperatorWithSymbol(this.selectedConstraint.type);
+          console.log(document.getElementById('inputConfigValuesContainer' + index));
+          const valuesContainer = document.getElementById('inputConfigValuesContainer' + index);
 
-            this.clearContainer(valuesContainer);
-            const userConstraintConfigInput = this.drawConfig(valuesContainer);
+          this.clearContainer(valuesContainer);
+          const userConstraintConfigInput = this.drawConfig(valuesContainer);
 
-            // Attach an event listener to the select element to retrieve user input in ConstraintConfig
-            const getSelectedValue = () => {
-              const selectedValueJSON = userConstraintConfigInput();
-              // Do something with the selected value, e.g., log it, pass it to another function, etc.
-              this.constraintsConfig[this.selectedConstraint.name] = selectedValueJSON;
-            };
+          // Attach an event listener to the select element to retrieve user input in ConstraintConfig
+          const getSelectedValue = () => {
+            const selectedValueJSON = userConstraintConfigInput();
+            // Do something with the selected value, e.g., log it, pass it to another function, etc.
+            this.constraintsConfig[this.selectedConstraint.name] = selectedValueJSON;
+          };
 
-            // Add the event listener to the select element
-            valuesContainer.addEventListener('change', getSelectedValue); 
+          // Add the event listener to the select element
+          valuesContainer.addEventListener('change', getSelectedValue); 
         },
         drawConfig(container){
           var userConstraintConfigInput = null;
@@ -81,7 +86,14 @@
             }
             return userConstraintConfigInput
         },
-
+        addConstraint() {
+          this.constraintInstances.push({
+            selectedConstraint: null,
+          });
+        },
+        removeConstraint(index) {
+          this.constraintInstances.splice(index, 1);
+        },
     },
     mounted() {
         this.fetchAllConstraints();
