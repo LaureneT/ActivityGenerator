fabfdce853a7c0fde40691c954c744e1a7a2e97b
Laurène Tavella <59617674+LaureneT@users.noreply.github.com>
2023-08-01 12:06:04 -0400

    create DrawAndSetup fct in INOperator

--- frontend/src/operators/InOperator.js
+++ frontend/src/operators/InOperator.js
@@ -70,6 +70,46 @@ export class InOperator {
         // Return the lambda expression
         return getUserInput;
     }
+
+    drawAndsetConfig(container, values, constraintConfig){
+        const valuesJSON = JSON.parse(values);
+
+        // Create the select (dropdown) element
+        const selectElement = document.createElement('select');
+        selectElement.name = 'selectedOption';
+        
+        // Add an empty option as the default "void" selection
+        const defaultOption = document.createElement('option');
+        defaultOption.value = ''; // You can set the value to null or any other value if needed
+        defaultOption.textContent = 'Select an option'; // You can provide any appropriate text here
+        selectElement.appendChild(defaultOption);
+
+        // Add options to the select element
+        valuesJSON.forEach((option) => {
+            const optionElement = document.createElement('option');
+            optionElement.value = option;
+            optionElement.textContent = option;
+            selectElement.appendChild(optionElement);
+        });
+        
+        // Set the selected value if it exists in constraintConfig
+        if (constraintConfig) {
+            const config = JSON.parse(constraintConfig);
+            selectElement.value = config;
+        }
+
+        // Append the select element to the container
+        container.appendChild(selectElement);   
+
+        // Lambda expression (arrow function) for capturing user input
+        const getUserInput = () => {
+            const selectedValue = selectElement.value;
+            return JSON.stringify(selectedValue);
+        };
+
+        // Return the lambda expression
+        return getUserInput;
+    }
 }
 
 
