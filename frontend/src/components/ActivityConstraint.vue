b8379b8f2a7645482718f140c7ee79f5f6834743
Laurène Tavella <59617674+LaureneT@users.noreply.github.com>
2023-08-07 10:57:52 -0400

    draw config

--- frontend/src/components/ActivityConstraint.vue
+++ frontend/src/components/ActivityConstraint.vue
@@ -5,12 +5,14 @@
       <select @change="ConstraintDropdownChanged" v-model="selectedConstraint">
         <option v-for="option in constraintsDropdownOptions" :value="option.name" :key="option.name">{{ option.name }}</option>
       </select>
+      <div :id="uniqueId"></div>  
     </form>
   </div>
 </template>
   
 <script>
 //import { ConstraintConfig } from "./ConstraintConfig";
+import { GetOperatorWithSymbol } from '../operators/Operators.js'; 
 
 export default {
   props:{
@@ -19,6 +21,7 @@ export default {
   },
   data() {
     return {
+      uniqueId: "div_" + Math.random().toString(36), // Generate a unique ID
       selectedConstraint: null,
       modifiedConfig: this.config,
     };
@@ -30,12 +33,27 @@ export default {
       
       // change this.config name 
       this.modifiedConfig.constraintName = selectedConstraint.name;
+      // draw config
+      const valuesContainer = document.getElementById(this.uniqueId); 
+      const selectedOperator = GetOperatorWithSymbol(selectedConstraint.type);
+      this.drawConfig(valuesContainer, selectedConstraint, selectedOperator);
       // change this.config data 
-      
+
       this.EmitModifiedConfig();
     },
     EmitModifiedConfig(){
       this.$emit('config-updated', this.modifiedConfig);
+    },
+    drawConfig(container, constraint, operator){
+      var userConstraintConfigInput = null;
+      if (constraint.values){
+        const valuesJSON = JSON.parse(constraint.values);
+        userConstraintConfigInput = operator.drawConfig(container, valuesJSON);
+      }
+      else{
+        userConstraintConfigInput = operator.drawConfig(container);
+      }
+      return userConstraintConfigInput
     },
   },
   mounted() {
@@ -43,6 +61,8 @@ export default {
     {
       this.selectedConstraint = this.config.constraintName;
     }
+    //idem for configdata
+
   },
 };
 </script>
