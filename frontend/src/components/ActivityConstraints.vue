96409d9f0f15fcc10235252d8c9609b8ca5be603
Laurène Tavella <59617674+LaureneT@users.noreply.github.com>
2023-08-07 10:20:10 -0400

    Update ActivityConstraints.vue

--- frontend/src/components/ActivityConstraints.vue
+++ frontend/src/components/ActivityConstraints.vue
@@ -1,149 +1,65 @@
 <template>
     <div>
       <h4>Add Constraints</h4>
-      <form @submit.prevent="createActivity">
+      <form>
         <button type="button" @click="addConstraintConfig">+ Add a constraint to the activity</button>
-        <div v-for="(constraint, index) in constraintsConfig" :key="index">
-          <label>Constraint: </label>
-          <select v-model="constraint.selectedConstraint" @change="onConstraintDropdownChange(index)">
-            <option v-for="option in constraintsDropdownOptions" :value="option" :key="option">{{ option.name }}</option>
-          </select>
-          <button type="button" @click="removeConstraint(index)">-</button>
+        <div v-for="(config, index) in configs" :key="index">
+            <activity-constraint :config=config @config-updated="updateParentConfig" :constraintsDropdownOptions="constraintsDropdownOptions" ></activity-constraint>
+          <!-- <button type="button" @click="removeConstraint(index)">-</button> -->
           <!-- Show fields based on the selected type of operator -->
-          <div :id="'inputConfigValuesContainer' + index"></div>      
+          <!-- <div :id="'inputConfigValuesContainer' + index"></div>       -->
         </div>
       </form>
     </div>
   </template>
   
   <script>
-    import api from '@/api.js';
-    import { GetOperatorWithSymbol } from '../operators/Operators.js'; 
-     // eslint-disable-next-line
-    import { ConstraintConfig } from "./ConstraintConfig";
+  import { ConstraintConfig } from "./ConstraintConfig";
+  import ActivityConstraint from "./ActivityConstraint.vue";
+  import api from '@/api.js';
 
   export default {
-    props: {
-        parentConstraintsConfigs: {
-          type: Array,
-          required: true,
-        },
-      },
+    components:{
+        ActivityConstraint,
+    },
+    // props: {
+    //     parentConstraintsConfigs: {
+    //       type: Array,
+    //       required: true,
+    //     },
+    //   },
     data() {
       return {
-        // constraintsConfig is a list of ConstraintConfig object
-        constraintsConfig: this.parentConstraintsConfigs == null ? [] : this.parentConstraintsConfigs, //Object.keys(this.parentConstraintsConfig).map((key) => this.parentConstraintsConfig[key]),
-        name: '',
+        configs: [new ConstraintConfig('Energy', {'Energy':'5'}), new ConstraintConfig()],
         constraintsDropdownOptions: [],
-        //activityConstraints: [],
       };
     },
     methods: {
       // Populate dropdown options
       async fetchAllConstraints(){
-          try {
-              const response = await api.get('/constraints');
-              this.constraintsDropdownOptions = response.data;
-          } catch (error) {
-              console.error('Error fetching constraints:', error);
-              throw new Error('An error occurred while fetching constraints.');
-          }
-      }, 
-      drawExistingConstraintsConfig(){
-        this.constraintsConfig.forEach((constraintConfig, index) => {
-          const selectedConstraint = constraintConfig.selectedConstraint;
-          const selectedOperator = GetOperatorWithSymbol(selectedConstraint.type);
-          const valuesContainer = document.getElementById('inputConfigValuesContainer' + index);
-          this.drawAndsetConfig(valuesContainer, selectedConstraint, selectedOperator, constraintConfig.constraintConfig);
-        })
-      },
-      addConstraintConfig() {
-        this.constraintsConfig.push({
-          constraintName : '',
-          constraintConfig : {},
-          selectedConstraint: null,
-        });
-      },
-      removeConstraint(index) {
-        const valuesContainer = document.getElementById('inputConfigValuesContainer' + index);
-        this.clearContainer(valuesContainer);
-        this.constraintsConfig.splice(index, 1);
-        this.drawExistingConstraintsConfig()
-      },
-      clearContainer(container){
-        if (container) {
-          while (container.firstChild) {
-            container.removeChild(container.firstChild);
-          }
+        try {
+          const response = await api.get('/constraints');
+          this.constraintsDropdownOptions = response.data;
+        } catch (error) {
+          console.error('Error fetching constraints:', error);
+          throw new Error('An error occurred while fetching constraints.');
         }
-        // const elementsToDelete = [...container.children];
-        // if (elementsToDelete){
-        //   elementsToDelete.forEach(element => {
-        //       console.log(element);
-        //       container.removeChild(element);
-        //   });
-        // }
+      }, 
+      addConstraintConfig(){
+        console.log('Adding 1 element to configs. Configs: ' + this.configs[0].constraintName);
       },
-      getConstraintsConfig() {
-        this.constraintsConfig.forEach((constraint, index) => {
-          const selectedConstraint = constraint.selectedConstraint;
-          const valuesContainer = document.getElementById('inputConfigValuesContainer' + index);
+      updateParentConfig(updatedConfig) {
+        console.log(updatedConfig);
+        console.log(updatedConfig.constraintName);
 
-          this.clearContainer(valuesContainer);
-          const userConstraintConfigInput = this.drawConfig(valuesContainer, selectedConstraint, GetOperatorWithSymbol(selectedConstraint.type));
-
-          const selectedValueJSON = userConstraintConfigInput(); // Get the user input from the dynamically generated fields
-          this.constraintsConfig[selectedConstraint.name] = selectedValueJSON;
-        });
-      return this.constraintsConfig;
-      },
-      // eslint-disable-next-line
-      onConstraintDropdownChange(index){
-        //this.getConstraintsConfig();
-        const selectedConstraint = this.constraintsConfig[index].selectedConstraint;
-        const selectedOperator = GetOperatorWithSymbol(selectedConstraint.type);
-        const valuesContainer = document.getElementById('inputConfigValuesContainer' + index); 
-        this.constraintsConfig[index].constraintName = selectedConstraint.name;
-
-        this.clearContainer(valuesContainer);
-        const userConstraintConfigInput = this.drawConfig(valuesContainer, selectedConstraint, selectedOperator);
-
-        // Attach an event listener to the select element to retrieve user input in ConstraintConfig
-        const getSelectedValue = () => {
-          const selectedValueJSON = userConstraintConfigInput();
-          // Do something with the selected value, e.g., log it, pass it to another function, etc.
-          this.constraintsConfig[index].constraintConfig = selectedValueJSON;
-        };
-
-        // Add the event listener to the select element
-        valuesContainer.addEventListener('change', getSelectedValue); 
-      },
-      drawConfig(container, constraint, operator){
-        var userConstraintConfigInput = null;
-          if (constraint.values){
-              const valuesJSON = JSON.parse(constraint.values);
-              userConstraintConfigInput = operator.drawConfig(container, valuesJSON);
-          }
-          else{
-              userConstraintConfigInput = operator.drawConfig(container);
-          }
-          return userConstraintConfigInput
-      },
-      drawAndsetConfig(container, constraint, operator, constraintConfig){
-        var userConstraintConfigInput = null;
-          if (constraint.values){
-              const valuesJSON = JSON.parse(constraint.values);
-              userConstraintConfigInput = operator.drawAndsetConfig(container, valuesJSON, constraintConfig);
-          }
-          else{
-              userConstraintConfigInput = operator.drawAndsetConfig(container, constraintConfig);
-          }
-          return userConstraintConfigInput
+        //console.log(updatedConfig.constraintName);
+        //console.log(index);
+        // Update the parentConfig with the modified data received from the child
+        //this.configs[index] = updatedConfig;
       },
     },
     mounted() {
       this.fetchAllConstraints();
-      this.drawExistingConstraintsConfig();
     },
   };
   </script>
